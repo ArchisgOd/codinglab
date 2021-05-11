4bfac2d64a61bcbf6ac892fd6a2e95008e8148e8
ArchisgOd <herostenti@mail.ru>
2021-05-11 19:09:03 +0600

    codinglab

---
+++ user_index2.php
@@ -0,0 +1,13 @@
+<?php
+    include "include/dbCon.php";
+    ?>
+    <!DOCTYPE html>
+<html>
+    <head>
+        <title>Profile</title>
+
+    </head>
+    <body style="background-color: red;">
+
+    </body>
+</html>
