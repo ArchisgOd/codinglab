ddaf824eb3f66770ed41628c135f43271c0e5816
ArchisgOd <herostenti@mail.ru>
2021-05-11 20:39:40 +0600

    codinglab

--- include/dbCon.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    $link = mysqli_connect('localhost', 'root','', 'register-bg');
-    if(mysqli_connect_errno()){
-        echo 'Ошибка к подключении  к базе данных ('.mysqli_connect_errno().'): '.mysqli_connect_error();
-        exit();
-    }
