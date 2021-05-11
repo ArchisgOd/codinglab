ae334052407c2f1708f5000bfb2d33ccf467e1ce
ArchisgOd <herostenti@mail.ru>
2021-05-11 18:15:12 +0600

    codinglab

--- checkerphp.php
+++ checkerphp.php
@@ -2,14 +2,19 @@
     require_once 'include/connetionphp.php';
     $login = filter_var(trim($_POST['login']),
     FILTER_SANITIZE_STRING);
+
     $name = filter_var(trim($_POST['name']),
     FILTER_SANITIZE_STRING);
+
     $surname = filter_var(trim($_POST['surname']),
     FILTER_SANITIZE_STRING);
+
     $email = filter_var(trim($_POST['email']),
     FILTER_SANITIZE_STRING);
+
     $uin = filter_var(trim($_POST['uin']),
     FILTER_SANITIZE_STRING);
+
     $pass = filter_var(trim($_POST['pass']),
     FILTER_SANITIZE_STRING);
 
