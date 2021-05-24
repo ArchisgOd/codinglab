cf34b9c575c0c9e7365daf2eb4813f1aabb4dc80
sssilencce <79984310+sssilencce@users.noreply.github.com>
2021-05-24 22:20:46 +0600

    codinglab

---
+++ addMatch.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style/login_style.css">
+</head>
+<body>
+<section id="menu">
+    <img src="https://img.icons8.com/ios-filled/50/ffffff/multiply.png" alt="exit" id="exitIcon">
+    <a href="index.php" class="menuList">Home</a>
+</section>
+
+<section id="all">
+    <section class="navbar">
+        <nav>
+            <div class="leftSide">
+                <div id="menuButton">
+                    <div class="menuLine"></div>
+                    <div class="menuLine"></div>
+                    <div class="menuLine"></div>
+                </div>
+                <a href="index.php" class="logo_box">TaDaNatBet</a>
+            </div>
+
+            <div class="rightSide">
+
+            </div>
+        </nav>
+    </section>
+
+    <section>
+        <h1>Add new match</h1>
+        <form method="post">
+            <label>
+                <input type="text" placeholder="White category" name="category" id="category">
+            </label>
+
+            <label>
+                <input type="text" placeholder="White first team" name="team1" id="team1">
+            </label>
+
+            <label>
+                <input type="text" placeholder="White second team" name="team2" id="team2">
+            </label>
+        </form>
+    </section>
+</section>
+
+<script src="js/jquery-3.5.1.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
+
