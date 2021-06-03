610cfad97afdeffbca0d780126aaef81643808cc
ArchisgOd <herostenti@mail.ru>
2021-06-03 10:28:31 +0600

    codinglab

--- backend/bet/addBetToTeam2.php
+++ backend/bet/addBetToTeam2.php
@@ -14,4 +14,16 @@ if (isset($_GET['id'])) {
 $resultUser = mysqli_query($mysqli, "SELECT * FROM  `users` where `login`='$user' ");
 $resultUser = mysqli_fetch_assoc($resultUser);
 
+if ($betTeam2 > $resultUser['balance'] || $betTeam2 < '0') {
+    echo '<script> 
+             alert("INSUFFICIENT FUNDS");
+             window.location.href="/codinglab/index.php";
+          </script>';
+} else {
+    $mysqli->query("UPDATE `users` SET `balance` = balance - '$betTeam2' WHERE `login` = '$user'");
+    $mysqli->query("INSERT INTO `bettomatchesstory` (`ID`, `login`, `matchID`, `paidMoney`, `team`, `tournament`) VALUES (NULL, '$user', '" . $matchResult['ID'] . "', '$betTeam2', '" . $matchResult['teamName2'] . "', '" . $matchResult['tournament'] . "')");
+    $mysqli->query("UPDATE `matches` SET `moneyTeam2` = moneyTeam2 + '$betTeam2' WHERE `ID` = '" . $matchResult['ID'] . "'");
 
+
+    header('Location: /codinglab/index.php');
+}
